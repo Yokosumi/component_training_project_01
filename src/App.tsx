19a307557a9d676b48bb35873a3b0043505053d8
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-12-28 17:34:53 +0100

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { NavBar } from './components/NavBar'
-
 function App() {
     return (
         <>
-            <div className="flex flex-col gap-10 justify-center items-center text-white my-4">
-                <NavBar />
-            </div>
+            <div className="flex flex-col gap-10 justify-center items-center text-white my-4"></div>
         </>
     )
 }
